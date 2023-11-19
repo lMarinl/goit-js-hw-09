25af41e9d7f6f078532fd6df1d9d5f1886c1ea2b
lMarinl <136239542+lMarinl@users.noreply.github.com>
2023-11-19 11:04:47 +0000

    Deploying to gh-pages from @ lMarinl/goit-js-hw-09@fe6942e04c5b3694daaf387efcc229fdff3a0b60 🚀

---
+++ 01-color-switcher.b311a8b1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(function(){d=setInterval((()=>{t.disabled=!0,e.disabled=!1,document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0})),e.disabled=!0;
+//# sourceMappingURL=01-color-switcher.b311a8b1.js.map
